ddaf8e22b0212f7c4f29ffea0fb94f140e20c3a5
carlo negrini <carloschi23@libero.it>
2022-02-25 11:41:36 +0100

    Added photorealistic camera

--- src/camera.h
+++ src/camera.h
@@ -5,8 +5,10 @@
 class camera
 {
 public:
-	camera(const vec3& origin, const vec3& lookat, float fFov, float fAspectRatio) :
-		m_Origin(origin), m_fAspectRatio(fAspectRatio), m_fFov(fFov)
+	camera(const vec3& origin, const vec3& lookat, float fFov, float fAspectRatio, 
+		float fAperture, float fFocusDist) :
+		m_Origin(origin), m_fAspectRatio(fAspectRatio), m_fFov(fFov),
+		m_fLensRadius(fAperture / 2.0f), m_fFocusDist(fFocusDist)
 	{
 		float fTheta = m_fFov * (3.141592f / 180.0f);
 		float fWidth = std::tan(fTheta / 2.0f);
@@ -14,23 +16,38 @@ public:
 		float fViewportHeight = 2.0f * fWidth;
 		float fViewportWidth = fViewportHeight * m_fAspectRatio;
 
-		vec3 dir = (origin - lookat).norm();
-		vec3 side = cross(vec3(0, 1, 0), dir);
-		vec3 relative_up = cross(dir, side);
+		m_TargetToOrigin = (origin - lookat).norm();
+		m_Side = cross(vec3(0, 1, 0), m_TargetToOrigin);
+		m_RelativeUp = cross(m_TargetToOrigin, m_Side);
 
-		m_vVertical = relative_up * fViewportHeight;
-		m_vHorizontal = side * fViewportWidth;
+		m_vVertical = m_RelativeUp * fViewportHeight;
+		m_vHorizontal = m_Side * fViewportWidth;
 
-		m_vBottomLeft = -m_vHorizontal / 2.0f - m_vVertical / 2.0f - dir + origin;
+		m_vBottomLeft = -m_vHorizontal / 2.0f - m_vVertical / 2.0f - m_TargetToOrigin + origin;
 	}
 
 	ray GetRay(float u, float v) const
 	{
-		return ray(m_Origin, m_vBottomLeft + m_vHorizontal * u + m_vVertical * v - m_Origin);
-	}
+		vec3 vec = random_in_flat_z() * m_fLensRadius;
+		vec3 offset = m_Side * vec.x() + m_RelativeUp * vec.y();
 
+		return ray(m_Origin + offset, (m_vBottomLeft + m_vHorizontal * u + m_vVertical * v - m_Origin - offset / m_fFocusDist).norm());
+	}
+protected:
+	vec3 random_in_flat_z() const
+	{
+		while (true)
+		{
+			vec3 ret(random_float(-1.0f, 1.0f), random_float(-1.0f, 1.0f), 0.0f);
+			if (ret.length() > 1.0f)
+				continue;
+
+			return ret;
+		}
+	}
 private:
 	vec3 m_Origin;
 	vec3 m_vBottomLeft, m_vVertical, m_vHorizontal;
-	float m_fAspectRatio, m_fFov;
+	vec3 m_TargetToOrigin, m_Side, m_RelativeUp;
+	float m_fAspectRatio, m_fFov, m_fLensRadius, m_fFocusDist;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,6 @@ vec3 get_scene_color(const ray& r, const scene& world, int32_t max_iterations)
 		return get_scene_color(data.mat->scatter(r, data, color), world, max_iterations - 1) * color;
 	
 	vec3 versor = r.direction.norm();
-	/*float gradient = (versor.y() + 1.0f) * 0.5f;
-	vec3 color1(1.0f, 1.0f, 1.0f);
-	vec3 color2(0.5f, 0.7f, 1.0f);
-	return color1 * gradient + color2 * (1.0f - gradient);*/
 
 	//Handling lights
 
@@ -46,7 +42,7 @@ void output_color(std::ofstream& file, vec3 color)
 
 int main()
 {
-	const int width = 1280, height = 768;
+	const int width = 1920, height = 1080;
 	int32_t iterations = 10;
 	float fAspectRatio = (float)width / (float)height;
 	std::ofstream file("src/output.ppm");
@@ -55,7 +51,7 @@ int main()
 	file.write(header.c_str(), header.size());
 
 	//Framework logic
-	camera cam(vec3(0.0f, 3.0f, 10.0f), vec3(0.0f, 0.0f, -2.0f), 20.0f, fAspectRatio);
+	camera cam(vec3(0.0f, 3.0f, 10.0f), vec3(0.0f, 0.0f, -2.0f), 20.0f, fAspectRatio, 0.5f, 15.0f);
 
 	//Objects in the scene
 	scene s = scene::generate_random();
